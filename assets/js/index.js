cab2cd983ab60b6ae827ed32317453176c1264da
Juan Eduardo Castellon <jecastellon@gmail.com>
2025-06-22 21:36:06 -0400

    Cambios finales para evitar números negativos

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,25 +3,3 @@ precio = 400000
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
 
-cantidadSpan = document.querySelector(`#cantidad`)
-cantidad.innerHTML = Number(cantidadSpan.innerHTML)
-valortotal = document.querySelector('#valortotal')
-valortotal.innerHTML = precio * cantidadSpan.innerHTML
-
-//Me encontré con dos problemas en el ejercicio
-//cuyo método no está en las guías ni tampoco lo
-//vimos en clases. El primero es cómo hacer que
-//la página se actualice sola para que el monto
-//del total cambie. El segundo, cómo hacer que el
-//monto no pase a un número negativo. Me vi
-//obligado a investigar en internet y llegué a
-//esta solución:
-
-setInterval(`
-  cantidad = Number(cantidadSpan.innerHTML)
-  cantidad = Math.max(cantidad, 0)
-  cantidadSpan.innerHTML = cantidad
-  valortotal.innerHTML = cantidad * precio
-`, 100)
-
-
